1d5729aa0c8a9c5859db3f1cd0d11d865e548419
Matthijs Openneer <matthijs.openneer@gmail.com>
2018-09-13 20:21:33 +0200

    added example

--- pages/home.php
+++ pages/home.php
@@ -5,5 +5,10 @@ App::pageAuth(['user'], "login");
 // Example to get user
 $user = User::findById(1);
 
+$user = DB::getInstance()->prepare('SELECT * FROM users WHERE id = :id');
+$user->execute(['id' => 1]);
+$user = $user->fetchAll(PDO::FETCH_CLASS, 'User');
+
+dd($user);
 ?>
 
